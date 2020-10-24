da0d1220709a0205fe73644c210567cb3f1c8524
yukjaegyong <70316567+yjglab@users.noreply.github.com>
2020-10-25 01:17:46 +0900

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,26 +2,26 @@ const bleu = document.querySelector("#BLEU");
 const blanc = document.querySelector("#BLANC");
 const rouge = document.querySelector("#ROUGE");
 
-const backBleu = document.querySelector(".background__back-2");
+const back = document.querySelector(".background__back-2");
 
 
 function mouseOver_bleu() {
-    backBleu.src = "img/bg-back-bleu.jpg";
+    back.src = "img/bg-back-bleu.jpg";
 }
 function mouseOut_bleu() {
-    backBleu.src = "img/bg-back-2.jpg";
+    back.src = "img/bg-back-2.jpg";
 }
 function mouseOver_blanc() {
-    backBleu.src = "img/bg-back-blanc.jpg";
+    back.src = "img/bg-back-blanc.jpg";
 }
 function mouseOut_blanc() {
-    backBleu.src = "img/bg-back-2.jpg";
+    back.src = "img/bg-back-2.jpg";
 }
 function mouseOver_rouge() {
-    backBleu.src = "img/bg-back-rouge.jpg";
+    back.src = "img/bg-back-rouge.jpg";
 }
 function mouseOut_rouge() {
-    backBleu.src = "img/bg-back-2.jpg";
+    back.src = "img/bg-back-2.jpg";
 }
 
 function init() {
